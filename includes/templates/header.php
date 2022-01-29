1b2261460cb522f0c0f15c04335da7f99440953a
Carlos Cancines <Carlozc90@gmail.com>
2022-01-29 00:40:23 -0500

    BD CRUD propiedades progress

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -5,22 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Bienes Raices</title>
-    <link rel="stylesheet" href="build/css/app.css" />
+    <link rel="stylesheet" href="/build/css/app.css" />
   </head>
   <body>
     <header class="header <?php echo $inicio ? 'inicio' : ""; ?>">
       <div class="contenedor contenido-header">
         <div class="barra">
-          <a href="/"> <img src="build/img/logo.svg" alt="Imagen Logo" /> </a>
+          <a href="/"> <img src="/build/img/logo.svg" alt="Imagen Logo" /> </a>
 
           <div class="mobile-menu">
-            <img src="build/img/barras.svg" alt="icono menu responsive" />
+            <img src="/build/img/barras.svg" alt="icono menu responsive" />
           </div>
 
           <div class="derecha">
             <img
               class="dark-mode-boton"
-              src="build/img/dark-mode.svg"
+              src="/build/img/dark-mode.svg"
               alt="imagen Dark Mode"
             />
             <nav class="navegacion">
