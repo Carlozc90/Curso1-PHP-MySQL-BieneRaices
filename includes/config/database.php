1caaa9d0351cb9fbcdeb54379dd6740df4e384a2
Carlos Cancines <Carlozc90@gmail.com>
2022-03-16 09:49:39 -0500

    desploy

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectarDB() : mysqli{
-    $db = new mysqli('localhost','root','root','bienes_raices');
+    $db = new mysqli('localhost','root','root','bienes_raices',3306);
 
     if(!$db){
         echo "Error no se pudo conectar";
